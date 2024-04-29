19543795eeb61f5ff48746ec667ec1ca41305ca9
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-04-29 17:30:29 -0300

    add: study type tuple in typescript

---
+++ src/AULAS/A006/type-tuple.ts
@@ -0,0 +1,7 @@
+// Tuple
+const dataClient: [number, string, boolean?] = [1, "luiz"];
+console.log(dataClient);
+
+// Readonly array
+const arrayClientOne: readonly string[] = ["luiz", "john", "2"];
+const arrayClientTWO: ReadonlyArray<string> = ["luiz", "john", "2"];
